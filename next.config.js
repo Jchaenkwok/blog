5e6cbd6f5b5363c141277a628736e57b1adb4166
jchaenkwok <jchaenkwok@gmail.com>
2023-03-08 12:49:52 -0500

    Update config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  assetPrefix: isProd ? '/blog/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
